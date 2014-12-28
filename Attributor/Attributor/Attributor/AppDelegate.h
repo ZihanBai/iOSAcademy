86a30ae1be6492c6419ebcbb8234a7595a702fd9
Allen Kashiwa <flyingcoding@gmail.com>
2014-12-28 21:54:22 +0800

    Add Attributor project

---
+++ Attributor/Attributor/Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by baizihan on 12/28/14.
+//  Copyright (c) 2014 柏梓涵个人作品. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/Attributor/Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by baizihan on 12/28/14.
+//  Copyright (c) 2014 柏梓涵个人作品. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
