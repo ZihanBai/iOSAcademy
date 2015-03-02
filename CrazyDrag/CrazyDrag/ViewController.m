97b8808b70eca7f0dc25f50921131344df250ff9
Allen Kashiwa <flyingcoding@gmail.com>
2015-03-02 23:12:36 +0800

    use NSString

--- CrazyDrag/CrazyDrag/ViewController.m
+++ CrazyDrag/CrazyDrag/ViewController.m
@@ -21,6 +21,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    currentValue = 50;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -30,7 +31,10 @@
 
 - (IBAction)sliderMoved:(id)sender {
     UISlider *slider = (UISlider *)sender;
-    NSLog(@"滑动块儿的当前数值是%f",slider.value);
+//    //NSLog(@"滑动块儿的当前数值是%f",slider.value);
+//    currentValue = (int)lroundf(slider.value);
+    NSString *message = [NSString stringWithFormat:@"当前数值是%d",(int)lroundf(slider.value)];
+    [[[UIAlertView alloc] initWithTitle:@"你好，世界" message:message delegate:nil cancelButtonTitle:@"安大是个好学校" otherButtonTitles:@"我是安大的学生", nil]show];
 }
 - (IBAction)showAlert:(id)sender {
     [[[UIAlertView alloc] initWithTitle:@"你好，世界" message:@"天上要掉馅儿饼啦！！" delegate:nil cancelButtonTitle:@"我来接住" otherButtonTitles:@"不，我来接住", nil] show];
