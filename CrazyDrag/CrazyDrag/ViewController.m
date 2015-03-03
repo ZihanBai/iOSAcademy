a3e911dbf56d0880398dc360e5bbf2ed1fd603ed
Allen Kashiwa <flyingcoding@gmail.com>
2015-03-03 22:29:13 +0800

    use instance varible

--- CrazyDrag/CrazyDrag/ViewController.m
+++ CrazyDrag/CrazyDrag/ViewController.m
@@ -13,15 +13,19 @@
     int currentValue;
 }
 - (IBAction)showAlert:(id)sender;
-- (IBAction)sliderMoved:(id)sender;
+- (IBAction)sliderMoved:(UISlider *)sender;
+@property (strong, nonatomic) IBOutlet UISlider *slider;
+
 @end
 
 @implementation ViewController
 
+@synthesize slider;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    currentValue = 50;
+    currentValue = self.slider.value;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -29,14 +33,15 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)sliderMoved:(id)sender {
-    UISlider *slider = (UISlider *)sender;
+- (IBAction)sliderMoved:(UISlider *)sender {
+//    UISlider *slider = (UISlider *)sender;
 //    //NSLog(@"滑动块儿的当前数值是%f",slider.value);
-//    currentValue = (int)lroundf(slider.value);
-    NSString *message = [NSString stringWithFormat:@"当前数值是%d",(int)lroundf(slider.value)];
+//    currentValue = (int)lroundf(sender.value);
+    NSString *message = [NSString stringWithFormat:@"当前数值是%d",(int)lroundf(sender.value)];
     [[[UIAlertView alloc] initWithTitle:@"你好，世界" message:message delegate:nil cancelButtonTitle:@"安大是个好学校" otherButtonTitles:@"我是安大的学生", nil]show];
 }
 - (IBAction)showAlert:(id)sender {
     [[[UIAlertView alloc] initWithTitle:@"你好，世界" message:@"天上要掉馅儿饼啦！！" delegate:nil cancelButtonTitle:@"我来接住" otherButtonTitles:@"不，我来接住", nil] show];
 }
+
 @end
