3e225ca04f849adfba1f217291c7006445e6a380
Allen Kashiwa <flyingcoding@gmail.com>
2015-03-05 22:44:12 +0800

    add score and round count

--- CrazyDrag/CrazyDrag/ViewController.m
+++ CrazyDrag/CrazyDrag/ViewController.m
@@ -12,11 +12,15 @@
 {
     int currentValue;
     int targetValue;
+    int score;
+    int round;
 }
 - (IBAction)showAlert:(id)sender;
 - (IBAction)sliderMoved:(UISlider *)sender;
 @property (strong, nonatomic) IBOutlet UISlider *slider;
 @property (strong, nonatomic) IBOutlet UILabel *targetLabel;
+@property (strong, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (strong, nonatomic) IBOutlet UILabel *roundLabel;
 
 @end
 
@@ -24,31 +28,26 @@
 
 @synthesize slider;
 @synthesize targetLabel;
+@synthesize scoreLabel;
 
 -(void)startNewRound
 {
     targetValue = 1 + (arc4random() % 100);
     currentValue = 50;
     self.slider.value = currentValue;
+    round += 1;
 }
 
 -(int)getDeffence
 {
-    int deffrence;
-    if (currentValue > targetValue) {
-        deffrence = currentValue - targetValue;
-    }else if (currentValue < targetValue)
-    {
-        deffrence = targetValue - currentValue;
-    }else{
-        deffrence = 0;
-    }
-    return deffrence;
+    return abs(currentValue - targetValue);
 }
 
 -(void)updateLabels
 {
     self.targetLabel.text = [NSString stringWithFormat:@"%d",targetValue];
+    self.scoreLabel.text = [NSString stringWithFormat:@"%d",score];
+    self.roundLabel.text = [NSString stringWithFormat:@"%d",round];
 }
 
 - (void)viewDidLoad {
@@ -70,7 +69,11 @@
 //    //NSLog(@"滑动块儿的当前数值是%f",slider.value);
 //    currentValue = (int)lroundf(sender.value);
     int deffrence = [self getDeffence];
-    NSString *message = [NSString stringWithFormat:@"当前数值是%d,我们的目标数值是%d,我们的差距是%d",(int)lroundf(sender.value),targetValue,deffrence];
+    int points = 100 - deffrence;
+    score += points;
+    
+//    NSString *message = [NSString stringWithFormat:@"当前数值是%d,我们的目标数值是%d,我们的差距是%d",(int)lroundf(sender.value),targetValue,deffrence];
+    NSString *message = [NSString stringWithFormat:@"您的得分是:%d",points];
     [[[UIAlertView alloc] initWithTitle:@"你好，世界" message:message delegate:nil cancelButtonTitle:@"安大是个好学校" otherButtonTitles:nil, nil]show];
     [self startNewRound];
     [self updateLabels];
