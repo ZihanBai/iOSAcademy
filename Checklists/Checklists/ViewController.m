d7dbf22b11ab86dd39cf5334c02040e836d0dcf0
Allen Kashiwa <flyingcoding@gmail.com>
2015-03-12 22:00:07 +0800

    use datasource and delegate

--- Checklists/Checklists/ViewController.m
+++ Checklists/Checklists/ViewController.m
@@ -26,13 +26,37 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 1;
+    return 100;
 }
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    UILabel *label = (UILabel *)[cell viewWithTag:1000];
+    if (indexPath.row % 5 == 0) {
+        label.text = @"你有如下待办事项：";
+    }else if (indexPath.row % 5 == 1)
+    {
+        label.text = @"吃大餐";
+    }else if (indexPath.row % 5 == 2){
+        label.text = @"洗澡";
+    }else if (indexPath.row % 5 == 3){
+        label.text = @"上班";
+    }else if (indexPath.row % 5 == 4){
+        label.text = @"逛动物园";
+    }
     return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if (cell.accessoryType == UITableViewCellAccessoryNone) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }else{
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 @end
