884eebca2b0ce1467567f1bf780e65b9bf713c2e
Allen Kashiwa <flyingcoding@gmail.com>
2015-03-13 21:21:24 +0800

    use mvc and add datasource

--- Checklists/Checklists/ViewController.m
+++ Checklists/Checklists/ViewController.m
@@ -12,11 +12,27 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSString *_row0text;
+    NSString *_row1text;
+    NSString *_row2text;
+    NSString *_row3text;
+    NSString *_row4text;
+    BOOL _row0checked;
+    BOOL _row1checked;
+    BOOL _row2checked;
+    BOOL _row3checked;
+    BOOL _row4checked;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _row0text = @"你有如下待办事项：";
+    _row1text = @"吃大餐";
+    _row2text = @"洗澡";
+    _row3text = @"上班";
+    _row4text = @"逛动物园";
+    _row4checked = YES;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -26,7 +42,27 @@
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 100;
+    return 5;
+}
+
+-(void)configureCheckmarkForCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath{
+    BOOL isChecked = NO;
+    if (indexPath.row == 0) {
+        isChecked = _row0checked;
+    }else if (indexPath.row == 1){
+        isChecked = _row1checked;
+    }else if (indexPath.row == 2){
+        isChecked = _row2checked;
+    }else if (indexPath.row == 3){
+        isChecked = _row3checked;
+    }else if (indexPath.row == 4){
+        isChecked = _row4checked;
+    }
+    if (isChecked) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }else{
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
 }
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -34,28 +70,42 @@
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
     UILabel *label = (UILabel *)[cell viewWithTag:1000];
     if (indexPath.row % 5 == 0) {
-        label.text = @"你有如下待办事项：";
+        label.text = _row0text;
     }else if (indexPath.row % 5 == 1)
     {
-        label.text = @"吃大餐";
+        label.text = _row1text;
     }else if (indexPath.row % 5 == 2){
-        label.text = @"洗澡";
+        label.text = _row2text;
     }else if (indexPath.row % 5 == 3){
-        label.text = @"上班";
+        label.text = _row3text;
     }else if (indexPath.row % 5 == 4){
-        label.text = @"逛动物园";
+        label.text = _row4text;
     }
+    [self configureCheckmarkForCell:cell atIndexPath:indexPath];
     return cell;
 }
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
-    if (cell.accessoryType == UITableViewCellAccessoryNone) {
-        cell.accessoryType = UITableViewCellAccessoryCheckmark;
-    }else{
-        cell.accessoryType = UITableViewCellAccessoryNone;
+    BOOL isChecked = NO;
+    if (indexPath.row == 0) {
+        isChecked = _row0checked;
+        _row0checked = !_row0checked;
+    }else if (indexPath.row == 1){
+        isChecked = _row1checked;
+        _row1checked = !_row1checked;
+    }else if (indexPath.row == 2){
+        isChecked = _row2checked;
+        _row2checked = !_row2checked;
+    }else if (indexPath.row == 3){
+        isChecked = _row3checked;
+        _row3checked = !_row3checked;
+    }else if (indexPath.row == 4){
+        isChecked = _row4checked;
+        _row4checked = !_row4checked;
     }
+    [self configureCheckmarkForCell:cell atIndexPath:indexPath];
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
