f0428e0d19092f6ccb418db1979d8c317dff26af
Allen Kashiwa <flyingcoding@gmail.com>
2015-03-15 13:21:03 +0800

    add a new ViewController for adding item

---
+++ Checklists/Checklists/AddItemViewController.h
@@ -0,0 +1,15 @@
+//
+//  AddItemViewController.h
+//  Checklists
+//
+//  Created by baizihan on 3/15/15.
+//  Copyright (c) 2015 柏梓涵个人作品. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddItemViewController : UITableViewController
+- (IBAction)cancel:(id)sender;
+- (IBAction)done:(id)sender;
+
+@end
