343d70c3580ed0045c7b5454fe56a48797a01b9f
Allen Kashiwa <flyingcoding@gmail.com>
2015-03-23 22:07:30 +0800

    add new project
    a new project for studying location and core data

---
+++ MyLocations/MyLocations/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLocations
+//
+//  Created by baizihan on 3/23/15.
+//  Copyright (c) 2015 柏梓涵个人作品. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyLocations/MyLocations/FirstViewController.m
@@ -0,0 +1,27 @@
+//
+//  FirstViewController.m
+//  MyLocations
+//
+//  Created by baizihan on 3/23/15.
+//  Copyright (c) 2015 柏梓涵个人作品. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
